feafea93498eecc038c8df1da7a560ce10f0398b
Chuseok22 <bjh59629@naver.com>
2025-08-04 21:39:35 +0900

    회원 관련 기능 세팅 : feat : ShopFilteredRequest 생성자 오류 해결 https://github.com/Mealz-open/mealz-server/issues/3

--- src/main/java/com/mealz/server/domain/shop/application/dto/request/ShopFilteredRequest.java
+++ src/main/java/com/mealz/server/domain/shop/application/dto/request/ShopFilteredRequest.java
@@ -1,10 +1,12 @@
 package com.mealz.server.domain.shop.application.dto.request;
 
+import com.mealz.server.domain.shop.core.constant.ShopSortField;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.domain.Sort;
 
 @Getter
 @Setter
@@ -13,4 +15,7 @@ import lombok.Setter;
 @NoArgsConstructor
 public class ShopFilteredRequest {
 
+  private ShopSortField sortField;
+
+  private Sort.Direction sortDirection;
 }
