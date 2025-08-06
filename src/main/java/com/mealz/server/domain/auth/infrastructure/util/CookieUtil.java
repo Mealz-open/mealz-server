f363da666efbab9f10583847bd451a58807db8c4
Chuseok22 <bjh59629@naver.com>
2025-08-07 00:10:26 +0900

    더미 데이터 추가 : feat : 쿠키 SameSite Lax 변경 https://github.com/Mealz-open/mealz-server/issues/15

--- src/main/java/com/mealz/server/domain/auth/infrastructure/util/CookieUtil.java
+++ src/main/java/com/mealz/server/domain/auth/infrastructure/util/CookieUtil.java
@@ -63,7 +63,7 @@ public class CookieUtil {
         .path("/")
         .httpOnly(false)
         .secure(false)
-        .sameSite("None")
+        .sameSite("Lax")
         .maxAge(expirationTimeInSeconds)
         .build();
   }
@@ -78,7 +78,7 @@ public class CookieUtil {
         .path("/")
         .httpOnly(true)
         .secure(false)
-        .sameSite("None")
+        .sameSite("Lax")
         .maxAge(expirationTimeInSeconds)
         .build();
   }
